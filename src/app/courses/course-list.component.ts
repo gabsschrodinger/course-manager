ca16ec91191128db9b79719fb4f0ce2f92d39802
Gabriel Fernandes Ribeiro <contato.gfernandesribeiro@outlook.com>
2021-03-18 12:23:36 -0300

    Adding nav, pipe and 404

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,35 +1,31 @@
 import { Component, OnInit } from "@angular/core";
+import { CourseService } from "./course.service";
 import { Course } from "./couse";
 
 @Component({
-    selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit {
-    courses: Course[] = [];
+
+    filteredCourses: Course[] = [];
+
+    _courses: Course[] = [];
+
+    _filterBy: string;
+
+    constructor(private courseService: CourseService) { }
 
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Intro to Angular',
-                imageUrl: '/assets/img/angular.png',
-                price: 12.50,
-                code: 'ANG-001',
-                duration: 120,
-                rating: 4.5,
-                releaseDate: 'December, 15, 2020'
-            },
-            {
-                id: 2,
-                name: 'Intro to HTML',
-                imageUrl: '/assets/img/html.png',
-                price: 9.50,
-                code: 'WEB-001',
-                duration: 90,
-                rating: 3.5,
-                releaseDate: 'July, 8, 2020'
-            }
-        ]
+        this._courses = this.courseService.retrieveAll();
+        this.filteredCourses = this._courses;
+    }
+
+    set filter(value: string) {
+        this._filterBy = value;
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+    }
+
+    get filter() {
+        return this._filterBy;
     }
 }
